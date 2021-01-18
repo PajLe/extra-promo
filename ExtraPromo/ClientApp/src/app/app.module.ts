97f78e7aedb8f348c58b0d3117fbc091c5425198
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-01-18 01:38:23 +0100

    added ng bootstrap and some material modules

--- ExtraPromo/ClientApp/src/app/app.module.ts
+++ ExtraPromo/ClientApp/src/app/app.module.ts
@@ -9,8 +9,14 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -28,9 +34,15 @@ import { MatSliderModule } from '@angular/material/slider';
     { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'counter', component: CounterComponent },
     { path: 'fetch-data', component: FetchDataComponent },
-], { relativeLinkResolution: 'legacy' }),
-    NoopAnimationsModule,
-    MatSliderModule
+    ], { relativeLinkResolution: 'legacy' }),
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
