8dc0921bf7ba6f27b39472f85116e82ecc2ed967
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-01-19 22:47:09 +0100

    added ';' in home.component.ts

--- ExtraPromo/ClientApp/src/app/home/home.component.ts
+++ ExtraPromo/ClientApp/src/app/home/home.component.ts
@@ -30,7 +30,7 @@ export class HomeComponent implements OnInit {
     this.loginForm = this._formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
-    })
+    });
   }
 
   login(): void {
