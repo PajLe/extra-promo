86a1a04a4a30226f6272138d7f3bc094536abee9
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-01-21 16:51:30 +0100

    added promotion-related DTOs on backend for adding promotion

---
+++ ExtraPromo/DTOs/PromotionActionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtraPromo.DTOs
+{
+    public class PromotionActionDto
+    {
+        public string Type { get; set; }
+
+        public string[] Items { get; set; }
+
+        public double? Flat { get; set; }
+
+        public double? Percentage { get; set; }
+
+        public bool? FreeShip { get; set; }
+    }
+}
---
+++ ExtraPromo/DTOs/PromotionModifierDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ExtraPromo.DTOs
+{
+    public class PromotionModifierDto
+    {
+        public string Type { get; set; }
+
+        public string[] Values { get; set; }
+    }
+}
