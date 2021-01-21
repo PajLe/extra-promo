b83b700424da73504a21fd08119287e3e4db2438
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-01-21 22:57:03 +0100

    added database Promotion model

---
+++ ExtraPromo/DbSetup/Cassandra/Models/PromotionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExtraPromo.DbSetup.Cassandra.Models
+{
+    public class PromotionModel
+    {
+        public Guid Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+
+        public Dictionary<Guid, string> Modifiers { get; set; }
+
+        public Dictionary<Guid, string> Actions { get; set; }
+    }
+}
