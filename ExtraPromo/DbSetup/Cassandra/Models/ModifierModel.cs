4b27d93ef35aef048fe52d7fa8ec03fafb53837a
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-01-22 18:48:46 +0100

    added Cassandra ModifierModel

---
+++ ExtraPromo/DbSetup/Cassandra/Models/ModifierModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExtraPromo.DbSetup.Cassandra.Models
+{
+    public class ModifierModel
+    {
+        public Guid Id { get; set; }
+
+        public string Type { get; set; }
+
+        public IEnumerable<string> Values { get; set; }
+    }
+}
