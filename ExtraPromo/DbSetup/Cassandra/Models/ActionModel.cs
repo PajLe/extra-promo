ce27c10cf396f40c0ed0b6bcbfdfa8a9aa5f5607
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-01-22 19:06:19 +0100

    added Cassandra ActionModel

---
+++ ExtraPromo/DbSetup/Cassandra/Models/ActionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExtraPromo.DbSetup.Cassandra.Models
+{
+    public class ActionModel
+    {
+        public Guid Id { get; set; }
+
+        public double Flat { get; set; }
+
+        public bool FreeShip { get; set; }
+
+        public double Percentage { get; set; }
+
+        public IEnumerable<string> Items { get; set; }
+    }
+}
